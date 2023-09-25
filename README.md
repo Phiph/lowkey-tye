0c00d0f436544c9aab17ce3b4828151974ab6ee2
Phill Morton <me@phillmorton.co.uk>
2023-09-25 23:50:41 +0100

    add working sample

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # lowkey-tye
+
+
+This is a rough and ready PoC - not intended for production use. just an experiment! (Don't just my code too hard)
--- src/LowKey.API/Controllers/SecretsController.cs
+++ src/LowKey.API/Controllers/SecretsController.cs
@@ -1,6 +1,7 @@
 using Azure.Core;
 using Azure.Core.Pipeline;
 using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,7 @@ public class SecretsController : ControllerBase
     
     private readonly ILogger<SecretsController> _logger;
     private SecretClient _secretClient;
+    private readonly KeyClient _keyClient;
 
     public SecretsController(ILogger<SecretsController> logger)
     {
@@ -21,7 +23,12 @@ public class SecretsController : ControllerBase
         {
             DisableChallengeResourceVerification = true
         };
+        var keyoptions = new KeyClientOptions(KeyClientOptions.ServiceVersion.V7_4)
+        {
+            DisableChallengeResourceVerification = true
+        };
         _secretClient = new SecretClient(new Uri("https://localhost:8443/"),  new NoopCredentials(), GetClientOptions(options));
+        _keyClient = new KeyClient(new Uri("https://localhost:8443/"),  new NoopCredentials(), GetClientOptions(keyoptions));
     }
 
     [HttpPost]
@@ -37,10 +44,20 @@ public class SecretsController : ControllerBase
     public IActionResult Post(string name)
     {
         var secret = _secretClient.GetSecret(name);
+        
         return Ok(secret.Value);
     }
-     
     
+    
+    [HttpPost]
+    [Route("GetRSAKey")]
+    public IActionResult PostKey(string name)
+    {
+        var secret = _keyClient.GetKey(name);
+        
+        return Ok(secret.Value);
+    }
+
     
     private T GetClientOptions<T>(T options) where T : ClientOptions
     {
